655800dcc777cece954ab28e302a33aa51131f29
Jacqueline-Renito <al291218@edu.uaa.mx>
2024-04-10 09:53:39 -0600

    Vista Login
    Diseño de la pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { EntregaMeseroComponent } from './entrega-mesero/entrega-mesero.component';
 import { MesasReservaComponent } from './mesas-reserva/mesas-reserva.component';
 import { MesaCardComponent } from './mesa-card/mesa-card.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'entregaMesero', component: EntregaMeseroComponent},
-    { path: 'reservacionMesas', component: MesasReservaComponent}
+    { path: 'reservacionMesas', component: MesasReservaComponent},
+    { path: 'login', component: LoginComponent }
 ];
