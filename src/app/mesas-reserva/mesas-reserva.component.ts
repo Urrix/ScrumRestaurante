4db0ff675fc493c005bca6b243257c55b9cfe0c5
Jacqueline-Renito <al291218@edu.uaa.mx>
2024-04-25 00:10:01 -0600

    Correccion de Reservas

--- src/app/mesas-reserva/mesas-reserva.component.ts
+++ src/app/mesas-reserva/mesas-reserva.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MesaCardComponent } from '../mesa-card/mesa-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mesas-reserva',
   standalone: true,
-  imports: [MesaCardComponent],
+  imports: [MesaCardComponent,
+    CommonModule
+  ],
   templateUrl: './mesas-reserva.component.html',
   styleUrl: './mesas-reserva.component.css'
 })
-export class MesasReservaComponent {
+export class MesasReservaComponent implements OnInit {
+  
+  array:number[] = [1,2,3,4,5,6,7,8,9,10]
+  ngOnInit(): void {
+    console.log(this.array)
+  }
 
 }
